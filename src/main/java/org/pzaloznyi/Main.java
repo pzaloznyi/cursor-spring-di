bc4ed7e025e88f5133894e39ff13fa917c7c68ba
pavlo.zaloznyi <pavlo.zaloznyi@pm.bet>
2022-08-22 21:50:35 +0300

    java config

--- src/main/java/org/pzaloznyi/Main.java
+++ src/main/java/org/pzaloznyi/Main.java
@@ -6,16 +6,32 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        var beanFactory = new ClassPathXmlApplicationContext("config.xml");
-        var xmlConfigCalculatorService = (CalculatorService) beanFactory.getBean("calculatorService");
+        xmlInjectionDemo();
+        annotationInjectionDemo();
+        javaConfigInjectionDemo();
+    }
 
-        System.out.println(xmlConfigCalculatorService.calculate(25, 10));
-        System.out.println(xmlConfigCalculatorService.calculate(9, 10));
+    private static void xmlInjectionDemo() {
+        ClassPathXmlApplicationContext beanFactory = new ClassPathXmlApplicationContext("config.xml");
+        var calculatorService = (CalculatorService) beanFactory.getBean("calculatorService");
 
-        var appContext = new AnnotationConfigApplicationContext("org.pzaloznyi");
-        var calculator = (CalculatorService) appContext.getBean("calculatorService");
+        System.out.println(calculatorService.calculate(25, 10));
+        System.out.println(calculatorService.calculate(9, 10));
+    }
 
-        System.out.println(calculator.calculate(26, 10));
-        System.out.println(calculator.calculate(1, 38));
+    private static void annotationInjectionDemo() {
+        AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext("org.pzaloznyi");
+        var calculatorService = (CalculatorService) appContext.getBean("calculatorService");
+
+        System.out.println(calculatorService.calculate(26, 10));
+        System.out.println(calculatorService.calculate(1, 30));
+    }
+
+    private static void javaConfigInjectionDemo() {
+        AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext("org.pzaloznyi.config");
+        var calculatorService = appContext.getBean(CalculatorService.class);
+
+        System.out.println(calculatorService.calculate(1, 2));
+        System.out.println(calculatorService.calculate(2, 1));
     }
 }
